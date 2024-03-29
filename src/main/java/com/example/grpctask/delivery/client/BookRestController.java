5c41f2ba59028527be9c0ae74cf5bf44e1667279
sluchyk <boss.sluchyk@gmail.com>
2024-03-29 12:17:39 +0200

    add citrus test

--- src/main/java/com/example/grpctask/delivery/client/BookRestController.java
+++ src/main/java/com/example/grpctask/delivery/client/BookRestController.java
@@ -6,8 +6,6 @@ import com.example.grpctask.services.BookService;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
